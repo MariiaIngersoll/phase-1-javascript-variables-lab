90eef1016ea445de333f2b5d9b0e5772a9d685b8
+ <+>
2023-02-21 22:49:56 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+{
+    let companyName = 'Scuber';
+} 
+
+{const companyName = 'Scuber';}
+
+{
+    const mostProfitableNeighborhood = "Chelsea";
+}
+{
+  let mostProfitableNeighborhood = "Chealsea"; }
+
+  {
+    const companyCeo = "Susan Smith";
+  }
+  {
+    let companyCeo = "Susan Smith";
+  }
